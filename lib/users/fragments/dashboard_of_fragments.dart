20ba56e2275dddc707befe3a2a95bf5098115e08
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-08-18 21:28:35 +0300

    add navbare to dashboard

--- lib/users/fragments/dashboard_of_fragments.dart
+++ lib/users/fragments/dashboard_of_fragments.dart
@@ -1,11 +1,46 @@
+import 'package:chaina_globale_door/users/fragments/favorites_fragment_screen.dart';
+import 'package:chaina_globale_door/users/fragments/home_fragment_screen.dart';
+import 'package:chaina_globale_door/users/fragments/order_fragment_screen.dart';
+import 'package:chaina_globale_door/users/fragments/profile_fragment_screen.dart';
 import 'package:chaina_globale_door/users/userPreferences/current_user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // ignore: must_be_immutable, use_key_in_widget_constructors
 class DashboardOfFragments extends StatelessWidget {
   // ignore: prefer_final_fields
   CurrentUser _rememberCurrentUser = Get.put(CurrentUser());
+  List<Widget> _fragmentScreen = [
+    HomeFragmentScreen(),
+    FavoritesFragmentScreen(),
+    OrderFragmentScreen(),
+    ProfileFragmentScreen(),
+  ];
+
+  final List _navigationButtonProperties = [
+    {
+      "active_icon": Icons.home,
+      "none_active_icon": Icons.home_outlined,
+      "label": "Home",
+    },
+    {
+      "active_icon": Icons.favorite,
+      "none_active_icon": Icons.favorite_border,
+      "label": "Favorite",
+    },
+    {
+      "active_icon": FontAwesomeIcons.boxOpen,
+      "none_active_icon": FontAwesomeIcons.box,
+      "label": "Order",
+    },
+    {
+      "active_icon": Icons.person,
+      "none_active_icon": Icons.person_outlined,
+      "label": "Profile",
+    },
+  ];
+  RxInt _indexNumber = 0.obs;
   @override
   Widget build(BuildContext context) {
     return GetBuilder(
@@ -14,7 +49,31 @@ class DashboardOfFragments extends StatelessWidget {
         _rememberCurrentUser.getUserInfo();
       },
       builder: (controller) {
-        return const Scaffold();
+        return  Scaffold(
+          backgroundColor: Colors.lightBlue,
+          body: SafeArea(child: Obx(() => _fragmentScreen[_indexNumber.value]),),
+          bottomNavigationBar: Obx(
+            () => BottomNavigationBar(
+              currentIndex: _indexNumber.value,
+              onTap: (Value) {
+                _indexNumber.value = Value;
+              },
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white24,
+              items: List.generate(4, (index) {
+                var navBtnProperty = _navigationButtonProperties[index];
+                return BottomNavigationBarItem(
+                    backgroundColor: Colors.lightBlue,
+                    icon: Icon(navBtnProperty["none_active_icon"]),
+                    activeIcon: Icon(navBtnProperty["active_icon"]),
+                    label: navBtnProperty["label"],
+                );
+              }),
+            ),
+          ),
+        );
       },
     );
   }
